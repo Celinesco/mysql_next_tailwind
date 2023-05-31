e67a0cf35c84a5da9f5be640b35d00c690c205dc
celines <celina.alsina@gmail.com>
2023-05-31 16:38:12 +0200

    Displayed posts on front end. Styled form

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,30 +1,37 @@
 import axios from 'axios';
-
+import { useState } from 'react';
 
 export function PostForm() {
+
+    const [post, setPost] = useState({
+        title: "",
+        description:""
+      });
+      const handleChange = ({target: {name, value}}) => {
+        console.log(name, value);
+        setPost({...post, [name]: value})
+      }
 
    const  handleSubmit = async(e) => {
         e.preventDefault();
         console.log('creating product')
-        const res = await axios.post('/api/posts', {
-            title:'Second Post',
-            description: 'Este es el segundo post de Docker',
-        })
+        const res = await axios.post('/api/posts', post)
 
         console.log(res)
     }
     
     return (
-        <div className="bg-blue-500 p-10">
+        <div className="bg-blue-500 p-10  shadow-md rounded">
+            <h3 className='mb-3'>Enter your new post here</h3>
             <form onSubmit={handleSubmit} className="flex flex-col">
 
                 <label htmlFor="title">Title:</label>
-                <input className="my-2" type="text" id="title" name="title"></input>
+                <input className="my-2 rounded shadow border py-2" type="text" id="title" name="title" onChange={handleChange}></input>
                 <label htmlFor="description">Description</label>
-                <textarea  className="my-2" id='description' name="description"></textarea>
+                <textarea onChange={handleChange} className="my-2 rounded shadow border py-2" id='description' name="description"></textarea>
 
 
-                <button>Save</button>
+                <button className='bg-cyan-200 hover:bg-cyan-400 rounded focus:outline-none'>Save</button>
             </form>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import { PostForm } from '../components/PostForm';
 import axios from 'axios';
 
+
 export default function Home({ posts }) {
+
+
+
   console.log('Deberia imprimirse',posts)
   return (
     <main className="flex flex-col items-center justify-between pt-24">
       <h1 className='text-3xl font-bold '>Hello World</h1>
+      {posts.map(post=> (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+        </div>
+      ))}
       <PostForm />
     </main>
   )
