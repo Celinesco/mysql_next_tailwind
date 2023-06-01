c637f25322b5eaeb6e76bdefbe2bf6b8376874a0
celines <celina.alsina@gmail.com>
2023-06-01 17:14:01 +0200

    Changed html tags

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,13 @@ export default function Home({ posts }) {
   return (
     <Layout>
       <h1 className='text-3xl font-bold '>Hello World</h1>
+      <div className='w-full flex justify-end' >
+        <Link href={'/posts/newpost'}>
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">New Post</div>
+        </Link>
+       
+      </div>
+      
       {posts.map(post => (
         <div key={post.id} className='border border-gray-200 shadow my-6 p-10 flex justify-between bg-white'>
           <div>
@@ -27,17 +34,17 @@ export default function Home({ posts }) {
           </div>
           <div className='flex'>
             <Link href={`/posts/${post.id}`}>
-              <button type="button" className="text-purple-500  hover:bg-gray-100 focus:ring-1 focus:outline-none w-9 h-9 rounded-full p-2 flex items-center hover:shadow justify-center">
+              <div className="text-purple-500  hover:bg-gray-100 focus:ring-1 focus:outline-none w-9 h-9 rounded-full p-2 flex items-center hover:shadow justify-center">
                 <RiEyeFill className="w-5 h-5" />
-              </button>
+              </div>
             </Link>
             <button type="button" className="text-red-400  hover:bg-gray-100 focus:ring-1 focus:outline-none w-9 h-9 rounded-full p-2 flex items-center hover:shadow justify-center" onClick={()=>handleDelete(post.id)}>
               <TrashIcon />
             </button>
             <Link href={`/posts/edit/${post.id}`}>
-            <button type="button" className="text-yellow-400  hover:bg-gray-100 focus:ring-1  w-9 h-9 rounded-full p-2 flex items-center hover:shadow justify-center" >
+            <div className="text-yellow-400  hover:bg-gray-100 focus:ring-1  w-9 h-9 rounded-full p-2 flex items-center hover:shadow justify-center" >
               <PencilIcon />
-            </button>
+            </div>
             </Link>
            
           </div>
