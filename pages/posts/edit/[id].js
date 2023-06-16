e8004dd7ff0703a44dc71dd4cb8881c7db873015
celines <celina.alsina@gmail.com>
2023-06-16 13:24:32 +0200

    fix form post background

--- pages/posts/edit/[id].js
+++ pages/posts/edit/[id].js
@@ -1,10 +1,9 @@
-import Layout from "../../../components/Layout"
 import NewPost from "../newpost"
 
 export default function EditPost () {
     return (
-        <Layout>
+        <>
             <NewPost sectionTitle='Edit Post'/>
-        </Layout>
+        </>
     )
 }
--- pages/posts/newpost.js
+++ pages/posts/newpost.js
@@ -3,9 +3,9 @@ import { PostForm } from '../../components/PostForm'
 
 export default function NewPost ({sectionTitle = 'New Post'}) {
     return (
-        <>
+        <Layout>
           <h2 className="text-4xl my-10 text-blue-600 text-center font-bold">Edit post</h2>
             <PostForm />
-        </>
+        </Layout>
     )
 }
